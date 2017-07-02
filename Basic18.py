fb2e778157acc63e8d11a6c18fe03d7e652150e5
ehedaoo <ehedaoo@gmail.com>
2017-07-02 17:54:22 +0530

    Add all new files

--- Basic18.py
+++ Basic18.py
@@ -1,1 +1,3 @@
-# coding=utf-8
+# Write a Python program to get a new string from a given string
+# where "Is" has been added to the front.
+# If the given string already begins with "Is" then return the string unchanged.
